0188ec48512c127114904e63fdce658e6d50f2b6
GoshaLebowski <sirinkinegor@gmail.com>
2025-04-15 11:24:52 +0500

    Cтраница с настройками
    1. Создание файла Tabs.tsx, установка react-tabs из shadcn
    
    2. Создание файла Heading.tsx для разметки описание и загловки на странице настроеек в пенели управления
    
    3. Создание файла UserSettings.tsx установка разметки с использованием ui компонента из Tabs.tsx
    
    4. Изменения в файле /(site)/dashboard/settings/page.tsx и добавление компонента UserSettings.tsx

--- src/app/(site)/dashboard/settings/page.tsx
+++ src/app/(site)/dashboard/settings/page.tsx
@@ -1,11 +1,27 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+
+
+
+import { UserSettings } from '@/components/features/user/UserSettings';
+
+
+
+
 
 export async function generateMetadata(): Promise<Metadata> {
+	const t = await getTranslations(`dashboard.settings.header`)
+
 	return {
-		title: 'Панель управления'
+		title: t(`heading`),
+		description: t(`description`),
+		robots: {
+			index: false,
+			follow: false
+		}
 	}
 }
 
-export default function DashboardPage() {
-	return <div>Dashboard</div>
+export default function SettingsPage() {
+	return <UserSettings />
 }
