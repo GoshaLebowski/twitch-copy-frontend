d0f29e9a8aa187f437e9fc14d608e4e39ae33949
GoshaLebowski <sirinkinegor@gmail.com>
2025-04-28 09:58:35 +0500

    Управление сессиями
    1. Создание файлов запросов: FindCurrentSession.graphql запрос на текущею сессию. FindSessionByUser.graphql запрос на список сессий пользователя. RemoveSession.graphql запрос на удаление сессии пользователя
    
    2. Создание утилитарных файлов: format-date.ts функция для конвертации даты из форматы GMT. get-browser-icon.ts функция определения иконки с какого браузера клиент зашёл на сайт
    
    3. Создание компонентных файлов: SessionsList.tsx список актульных сессий и текущей сессии. SessionItem.tsx карточка сессии для вывода в SessionsList.tsx. SessionModal.tsx подробная информация о сессии располжение пользователя показывается на карте

--- src/components/ui/elements/CardContainer.tsx
+++ src/components/ui/elements/CardContainer.tsx
@@ -1,27 +1,48 @@
-import type { PropsWithChildren, ReactNode } from 'react'
+import type { LucideIcon } from 'lucide-react';
+import type { PropsWithChildren, ReactNode } from 'react';
+import type { IconType } from 'react-icons';
 
-import { Card } from '@/components/ui/common/Card'
+
+
+import { Card } from '@/components/ui/common/Card';
+
+
+
+
 
 interface CardContainerProps {
 	heading: string
 	description: string
+	Icon?: IconType | LucideIcon
 	rightContent?: ReactNode
 }
 
 export function CardContainer({
 	heading,
 	rightContent,
+	Icon,
 	description,
 	children
 }: PropsWithChildren<CardContainerProps>) {
 	return (
 		<Card className={`p-4`}>
 			<div className={`flex items-center justify-between`}>
-				<div className={`space-y-1`}>
-					<h2 className={`font-semibold tracking-wide`}>{heading}</h2>
-					<p className={`max-w-4xl text-sm text-muted-foreground`}>
-						{description}
-					</p>
+				<div className={`flex flex-row items-center gap-x-4`}>
+					{Icon && (
+						<div className={`rounded-full bg-foreground p-2.5`}>
+							<Icon className={`size-7 text-secondary`} />
+						</div>
+					)}
+					<div className={`space-y-1`}>
+						<h2 className={`font-semibold tracking-wide`}>
+							{heading}
+						</h2>
+						<p
+							className={`max-w-4xl text-sm text-muted-foreground`}
+						>
+							{description}
+						</p>
+					</div>
 				</div>
 				{rightContent && <div>{rightContent}</div>}
 			</div>
