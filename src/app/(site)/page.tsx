9f10f5a85196f12c56bac0361070862a332d37d4
GoshaLebowski <sirinkinegor@gmail.com>
2025-05-08 18:07:29 +0500

    Получение стримов на главной странице
    1. Создание файла запроса: FindRandomStreams.graphql список стримов
    
    2. Изменение файла страницы: src/app/(site)/page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,21 +1,44 @@
-'use client';
-
-import { useCurrent } from '@/hooks/useCurrent';
+import { FindRandomStreamsDocument, FindRandomStreamsQuery } from '@/graphql/generated/output';
 
 
 
+import { SERVER_URL } from '@/libs/constants/url.constants';
 
 
-export default function Home() {
-	const { user, isLoadingProfile } = useCurrent()
 
-	return (
-		<div>
-			{isLoadingProfile ? (
-				<div>Loading...</div>
-			) : (
-				<div>{JSON.stringify(user)}</div>
-			)}
-		</div>
-	)
+
+
+async function findRandomStreams() {
+	try {
+		const query = FindRandomStreamsDocument.loc?.source.body
+
+		const response = await fetch(SERVER_URL, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({ query }),
+			next: {
+				revalidate: 30
+			}
+		})
+
+		const data = await response.json()
+
+		return {
+			streams: data.data
+				.findRandomStreams as FindRandomStreamsQuery['findRandomStreams']
+		}
+	} catch (error) {
+		console.log(error)
+		throw new Error('Ошибка при получени стримов')
+	}
+}
+
+export default async function HomePage() {
+	const {streams} = await findRandomStreams();
+
+	return <div className={`space-y-10`}>
+		{JSON.stringify(streams, null, 2)}
+	</div>
 }
