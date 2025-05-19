f5dd63644c37f8f5fc83fe22f014feb2a02f6acc
GoshaLebowski <sirinkinegor@gmail.com>
2025-05-19 17:06:19 +0500

    Кнопка "Поддержать автора"
    1. Изменение файла с запросом: FindChannelByUsername.graphql добавление свойств для отображение в канале
    
    2. Создание файла с запросом: FindSponsorsByChannel.graphql поиск спонсорского пользователя на канале. MakePayment.graphql ссылка на покупку плана
    
    3. Создание страницы: src/app/success/page.tsx (успешная операция по оплате)
    
    4. Создание компонента: SupportButton.tsx кнопка для выбора плана на канале и его оформление оплаты
    
    5. Изменение компонентов: StreamActions.tsx родительский компонент для FollowButton.tsx, SupportButton.tsx для оформление спонсорство. PlansTable.tsx исправление ошибки в запросе на удаление плана (ошибка в свойстве объекта перевода при инициализации ошибки в запросе на удаление). StreamInfo.tsx исправлено css свойство (кнопки на подписку и спонсорство теперь по центру)

--- src/components/features/stream/overview/info/StreamActions.tsx
+++ src/components/features/stream/overview/info/StreamActions.tsx
@@ -1,5 +1,13 @@
-import type { FindChannelByUsernameQuery } from '@/graphql/generated/output'
-import { FollowButton } from '@/components/features/stream/overview/info/FollowButton'
+import { FollowButton } from '@/components/features/stream/overview/info/FollowButton';
+import { SupportButton } from '@/components/features/stream/overview/info/SupportButton';
+
+
+
+import type { FindChannelByUsernameQuery } from '@/graphql/generated/output';
+
+
+
+
 
 interface StreamActionsProps {
 	channel: FindChannelByUsernameQuery[`findChannelByUsername`]
@@ -10,7 +18,10 @@ export function StreamActions({ channel }: StreamActionsProps) {
 		<div
 			className={`mt-5 items-center space-x-3 space-y-4 lg:mt-0 lg:flex lg:space-y-0`}
 		>
-			<FollowButton channel={channel}/>
+			<FollowButton channel={channel} />
+			{channel.isVerified && channel.sponsorshipPlans.length && (
+				<SupportButton channel={channel} />
+			)}
 		</div>
 	)
 }
